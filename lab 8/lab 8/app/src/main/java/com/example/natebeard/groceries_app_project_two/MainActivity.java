161c433851414b6b26538f5ef888669b940eb815
natebeard <natebeard04@gmail.com>
2017-04-20 18:33:54 -0600

    lab 8
    This is almost project 2, but rebuilt the groceries app with fragments
    and figured it would work for lab 8 too

---
+++ lab 8/lab 8/app/src/main/java/com/example/natebeard/groceries_app_project_two/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.natebeard.groceries_app_project_two;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
